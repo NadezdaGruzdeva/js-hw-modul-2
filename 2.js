71272a1c4cc10de9213d297b9b517337680aa747
NadezdaGruzdeva <ng2015note4@gmail.com>
2021-02-22 19:51:23 +0100

    Modul2

---
+++ 2.js
@@ -0,0 +1,45 @@
+//Проверка пароля (ранний возврат)
+//Задание
+//Функция checkPassword получает пароль пользователя в параметр password, проверяет его на совпадение с паролем администратора в переменной ADMIN_PASSWORD и возвращает сообщение о результате сравнения.
+//
+//Проведи рефакторинг кода функции checkStorage используя паттерн «ранний возврат»:
+//
+//удали переменную message;
+//удали else;
+//код должен работать так же, как и до оптимизации.
+//Тесты
+//Объявлена функция checkPassword(password).
+//Вызов checkPassword('mangohackzor') возвращает 'Доступ запрещен, неверный пароль!'.
+//Вызов checkPassword('polyhax') возвращает 'Доступ запрещен, неверный пароль!'.
+//Вызов checkPassword('jqueryismyjam') возвращает 'Добро пожаловать!'.
+//function checkPassword(password) {
+//  const ADMIN_PASSWORD = 'jqueryismyjam';
+//  // Пиши код ниже этой строки
+//  let message;
+//
+//  if (password === ADMIN_PASSWORD) {
+//    message = 'Добро пожаловать!';
+//  } else {
+//    message = 'Доступ запрещен, неверный пароль!';
+//  }
+//
+//  return message;
+//  // Пиши код выше этой строки
+//}
+
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  
+  
+  if (password === ADMIN_PASSWORD) {
+    
+    return 'Добро пожаловать!';
+  }
+  
+    return 'Доступ запрещен, неверный пароль!';
+ 
+
+  
+  
+}
+
---
+++ 21.js
@@ -0,0 +1,25 @@
+//Поиск самого длинного слова
+//Задание
+//Напиши фукцнию findLongestWord(string) которая принимает произвольную строку состоящую только из слов разделённых пробелом (параметр string) и возвращает самое длинное слово в этой строке.
+//
+//Тесты
+//Объявлена функция findLongestWord(string).
+//Вызов функции findLongestWord('The quick brown fox jumped over the lazy dog') возвращает jumped.
+//Вызов функции findLongestWord('Google do a roll') возвращает Google.
+//Вызов функции findLongestWord('May the force be with you') возвращает force.
+//Вызов функции findLongestWord() со случайной строкой возвращает правильное значение.
+function findLongestWord(string) {
+  // Пиши код ниже этой строки
+  let words = string.split(' ');
+  let longestWord = words[0];
+  
+  for (let word of words) {
+  
+  if (word.length > longestWord.length) {
+  longestWord = word;
+  }
+    
+  }
+return longestWord;
+  // Пиши код выше этой строки
+}
